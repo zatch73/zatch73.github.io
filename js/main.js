c83f181fbd672d9b4f2779711cb5a03200370e2c
zatch73 <marchiolimatheus@gmail.com>
2024-03-12 13:39:36 -0300

    backup

--- js/main.js
+++ js/main.js
@@ -24,14 +24,30 @@ fetch(`https://api.github.com/users/${repo_user}/repos`)
         project_dates.className = 'project_dates';
         let project_date1 = document.createElement('i');
         let project_date2 = document.createElement('i');
-    
+        let p_git =  document.createElement('p');
+        let p_deploy =  document.createElement('p');
+        let href_git = document.createElement('a');
+        let href_deploy = document.createElement('a');
+        href_git.textContent = 'Link to Github repository';
+        href_deploy.textContent = 'Link to deployment of application';
+        href_git.href = repository.html_url;
+        href_git.target = '_blank';
+        p_git.appendChild(href_git);
+
+        href_deploy.href = repository.homepage;
+        href_deploy.target = '_blank';
+        p_deploy.appendChild(href_deploy);
+
         project_date1.appendChild(document.createTextNode('Date realease: '+repository.created_at));
         project_date2.appendChild(document.createTextNode('Last update: '+repository.updated_at));
         project_dates.appendChild(project_date1)
         project_dates.appendChild(project_date2)
+
         project_box.appendChild(project_title);
         project_box.appendChild(project_description);
         project_box.appendChild(project_dates);
+        project_box.appendChild(p_git);
+        project_box.appendChild(p_deploy);
         document.getElementById('projects_box').appendChild(project_box);
     });
   })
